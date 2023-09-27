985b62b655d9f602b6dbcf7a222f53e6b1aad652
Jakub Waller <jakub.waller@protonmail.com>
2023-09-27 13:17:16 +0200

    certificates

--- src/components/Sections/Resume/index.tsx
+++ src/components/Sections/Resume/index.tsx
@@ -1,6 +1,6 @@
 import {FC, memo} from 'react';
 
-import {education, experience, certificates, SectionId, skills} from '../../../data/data';
+import {certificates, education, experience, SectionId, skills} from '../../../data/data';
 import Section from '../../Layout/Section';
 import ResumeSection from './ResumeSection';
 import {SkillGroup} from './Skills';
