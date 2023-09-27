c367b267059c5f97015ef46d56dd8f5b6c77b74e
Ernesto Wulff Olea <ernesto.wulffolea@azeti.net>
2023-09-27 16:56:43 +0200

    feat: store and intersection observer

--- src/components/Layout/Section.tsx
+++ src/components/Layout/Section.tsx
@@ -1,15 +1,30 @@
 import classNames from 'classnames';
-import {FC, memo, PropsWithChildren} from 'react';
+import {FC, memo, PropsWithChildren, useCallback} from 'react';
+import {InView} from 'react-intersection-observer';
 
 import {SectionId} from '../../data/data';
+import {useBearStore} from '../../store';
 
 const Section: FC<
   PropsWithChildren<{sectionId: SectionId; sectionTitle?: string; noPadding?: boolean; className?: string}>
 > = memo(({children, sectionId, noPadding = false, className}) => {
+  const {setSection} = useBearStore();
+
+  const handleOnChange = useCallback(
+    (visible: boolean, _: IntersectionObserverEntry) => {
+      if (visible) {
+        setSection(sectionId);
+      }
+    },
+    [sectionId, setSection],
+  );
+
   return (
-    <section className={classNames(className, {'px-4 py-16 md:py-24 lg:px-8': !noPadding})} id={sectionId}>
-      <div className={classNames({'mx-auto max-w-screen-lg': !noPadding})}>{children}</div>
-    </section>
+    <InView onChange={handleOnChange} threshold={0.2}>
+      <section className={classNames(className, {'px-4 py-16 md:py-24 lg:px-8': !noPadding})} id={sectionId}>
+        <div className={classNames({'mx-auto max-w-screen-lg': !noPadding})}>{children}</div>
+      </section>
+    </InView>
   );
 });
 
